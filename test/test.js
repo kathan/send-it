ef3452799722ba2ede613688da20fc79c1ddd85e
Darrel Kathan <Darrel.Kathan@grainger.com>
2018-03-09 11:27:39 -0600

    Added test

--- test/test.js
+++ test/test.js
@@ -4,21 +4,20 @@ const fs = require('fs');
 const express = require('express');
 const formidable = require('formidable');
 const app = express();
-const port = 4536;
+const port = 8080;
 
 //==== Start Server ====
 app.use((req, res, next)=>{
   if (req.method.toLowerCase() == 'post') {
     var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
+    form.parse(req, (err, fields, files) => {
       if(err){return;}
       req.body = fields;
       var files_obj = {};
-      for(var i in Object.keys(files)){
+      for(var i in files){
         files_obj[i] = fs.readFileSync(files[i].path);
       }
       req.files = files_obj;
-      console.log(files_obj);
       next();
     });
     return;
@@ -27,13 +26,13 @@ app.use((req, res, next)=>{
 });
 
 app.post('*', (req, res) => {
-  console.log({file_name: req.files.file.name});
+  console.log({files: req.files});
   res.send('Success!');
 });
 
 var server = app.listen(port, () => {
   //==== Send File ====
-  sendFile(`http://localhost:${port}`, path.resolve(__dirname, 'big.file'), (err, result, reply)=>{
+  sendFile(`http://localhost:${port}`, path.resolve(__dirname, 'test.file'), (err, result, reply)=>{
     if(reply.statusCode === 200){
       console.log('Success!');
     }else{
